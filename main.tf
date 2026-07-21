e82061273dfceebf9754a426c073678ff5f4c7d4
Dhondu <lakshyaright@gmail.com>
2026-07-21 21:19:42 +0530

    storage code added

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 resource "azurerm_resource_group" "rgs" {
-  name = "test_rg"
+  name     = "test_rg"
   location = "westus"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "stroageonetest1"
+  location                 = "westus"
+  resource_group_name      = "test_rg"
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
 }
