5cbd4f17d09c002ff00971047f4313bd194bb563
lakshyaright <lakshyaright@gmail.com>
2026-07-21 21:37:48 +0530

    Define Azure resource group and storage account
    Added Azure resource group and storage account resources in main.tf.

--- main.tf
+++ main.tf
@@ -18,3 +18,24 @@ resource "azurerm_storage_account" "storage" {
   account_tier              = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_resource_group" "rgs" {
+  name     = "test_rg"
+  location = "westus"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "stroageonetest1"
+  location                 = "westus"
+  resource_group_name      = "test_rg"
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "stroageonetest1"
+  location                 = "westus"
+  resource_group_name      = "test_rg"
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
+}
